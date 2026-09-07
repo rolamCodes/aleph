d5a629ed59fddc790a2a2c6c3bda12742853834e
rolam <rholem.abdellah@gmail.com>
2026-09-07 18:33:15 -0400

    Set up convex and clerk.

--- convex/_generated/api.js
+++ convex/_generated/api.js
@@ -1,10 +1,23 @@
 /* eslint-disable */
 /**
- * Generated API references.
+ * Generated `api` utility.
  *
- * This file will be replaced by `npx convex dev`.
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
  */
-import { anyApi } from "convex/server";
 
+import { anyApi, componentsGeneric } from "convex/server";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export const api = anyApi;
 export const internal = anyApi;
+export const components = componentsGeneric();
